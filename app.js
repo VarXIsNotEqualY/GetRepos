9ee28c8c8217781e6e19bdf7085b46367f8f8cea
VarXIsNotEqualY <40019113+VarXIsNotEqualY@users.noreply.github.com>
2018-06-16 13:59:18 +0300

    Changed XML to Fetch API, close #5

--- app.js
+++ app.js
@@ -1,25 +1,20 @@
-
 var n = document.getElementById('Uname');
-document.getElementById('button').addEventListener('click',fetchGithubRepos)
-    function fetchGithubRepos(){
-        var xhr = new XMLHttpRequest();
-            xhr.open('GET','https://api.github.com/users/'+n.value+'/repos',true)
-            
-            xhr.onload = function(){
-                if(this.status == 200){
-                    var repos = JSON.parse(this.responseText);
-                    
-                    var output = ''
+document.getElementById('button').addEventListener('click',fetchGithubRepos);
 
-                    for(var i in repos){
-                        output += 
-                        "<div class='user'>"+
-                        '<ul>' +
-                        '<li>'+repos[i].name+'</li>' +
-                        '</ul>'+'</div>'
-                } 
-            document.getElementById('repos').innerHTML = output;                   
-            }
-        }
-            xhr.send()
+function fetchGithubRepos(){
+    fetch('https://api.github.com/users/'+n.value+'/repos')
+    .then((res) => res.json())
+    .then((repos) => {
+        var output = '';
+
+    for(var i in repos){
+        output +="<div class='repos'>"+
+        '<ul>' +
+        '<li>'+repos[i].name+'</li>' +
+        '</ul>'+'</div>'
     }
+    document.getElementById('repos').innerHTML = output;
+      })
+     
+    
+}
