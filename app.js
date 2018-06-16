f2ad7b1d2becb0adc1049ac819be70af6a6ae6d1
VarXIsNotEqualY <40019113+VarXIsNotEqualY@users.noreply.github.com>
2018-06-16 14:42:39 +0300

    Made code more readable, close #1

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
-var n = document.getElementById('Uname');
-document.getElementById('button').addEventListener('click',fetchGithubRepos);
+var n = document.getElementById('Uname')
+document.getElementById('button').addEventListener('click', fetchGithubRepos)
 
-function fetchGithubRepos(){
-    fetch('https://api.github.com/users/'+n.value+'/repos')
+function fetchGithubRepos () {
+  fetch('https://api.github.com/users/' + n.value + '/repos')
     .then((res) => res.json())
     .then((repos) => {
-        var output = '';
+      var output = ''
 
-    for(var i in repos){
-        output +="<div class='repos'>"+
+      for (var i in repos) {
+        output += "<div class='repos'>" +
         '<ul>' +
-        '<li>'+repos[i].name+'</li>' +
-        '</ul>'+'</div>'
-    }
-    document.getElementById('repos').innerHTML = output;
-      })
-     
-    
+        '<li>' + repos[i].name + '</li>' +
+        '</ul>' + '</div>'
+      }
+      document.getElementById('repos').innerHTML = output
+    })
 }
