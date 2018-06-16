7896cc456d9b991d352bf074608b2b2c6e9a9044
VarXIsNotEqualY <40019113+VarXIsNotEqualY@users.noreply.github.com>
2018-06-16 13:19:19 +0300

    Renamed handler function, close #4

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
 
 var n = document.getElementById('Uname');
-    document.getElementById('button').addEventListener('click',lUs)
-function lUs(){
-    var xhr = new XMLHttpRequest();
-        xhr.open('GET','https://api.github.com/users/'+n.value+'/repos',true)
-        
-        xhr.onload = function(){
-            if(this.status == 200){
-                var repos = JSON.parse(this.responseText);
-                console.log(repos[1].name)
-                var output = ''
+document.getElementById('button').addEventListener('click',fetchGithubRepos)
+    function fetchGithubRepos(){
+        var xhr = new XMLHttpRequest();
+            xhr.open('GET','https://api.github.com/users/'+n.value+'/repos',true)
+            
+            xhr.onload = function(){
+                if(this.status == 200){
+                    var repos = JSON.parse(this.responseText);
+                    
+                    var output = ''
 
-                for(var i in repos){
-                    output += 
-                    "<div class='user'>"+
-                    '<ul>' +
-                    '<li>'+repos[i].name+'</li>' +
-                    '</ul>'+'</div>'
-            } 
-        document.getElementById('repos').innerHTML = output;                   
+                    for(var i in repos){
+                        output += 
+                        "<div class='user'>"+
+                        '<ul>' +
+                        '<li>'+repos[i].name+'</li>' +
+                        '</ul>'+'</div>'
+                } 
+            document.getElementById('repos').innerHTML = output;                   
+            }
         }
+            xhr.send()
     }
-        xhr.send()
-}
